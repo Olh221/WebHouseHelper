c826ba28ec12bb98d16549d261fecbbdb9201982
Oleh Petryshyn <olhpeterson.one@gmail.com>
2023-02-24 13:14:15 +0200

    Add project files.

---
+++ WebHouseHelper/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebHouseHelper.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
